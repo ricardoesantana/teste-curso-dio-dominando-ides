f95aef363b21387d57b778d1b4929f630080ea36
Ricardo Santana <ricardoesantana@gmail.com>
2022-10-11 17:48:00 -0300

    Primeiro Programa

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,7 +5,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		int a = 2;
 		int b = 3;
-		System.out.println("Hello World!"+ (a+b));
+		System.out.println("Hello World!" + (a + b));
 	}
 
 }
